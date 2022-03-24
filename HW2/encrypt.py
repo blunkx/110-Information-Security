4920b4131ef84d71bc8e6a6365a7b74c48bb7b0b
blunkx <asd9521889@gmail.com>
2022-03-24 12:25:01 +0800

    add main and des encrytion

--- HW2/encrypt.py
+++ HW2/encrypt.py
@@ -3,3 +3,30 @@ Introduction to Information Security
 HW2
 Encryption by B10830002
 """
+
+
+import sys
+import argparse
+
+
+def des_encryption(key, plaintext: str) -> str:
+    return "DES"
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        prog="decrypt.py",
+        description="decrypt ciphertext by the method and key entered by the user",
+    )
+    parser.add_argument("-i", "--input", help="the cyphertext to decrypt")
+    parser.add_argument("-k", "--key", help="the decryption key")
+    return parser
+
+
+def main():
+    args = get_parser().parse_args()
+    print(des_encryption(args.key, args.input))
+
+
+if __name__ == "__main__":
+    main()
