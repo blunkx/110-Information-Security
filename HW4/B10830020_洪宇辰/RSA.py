e5416fd4796ffddfde8a1562b970d4f71d5d522c
eric-hung-github <50710085+eric-hung-github@users.noreply.github.com>
2022-05-16 22:44:53 +0800

    comment

--- HW4/B10830020_洪宇辰/RSA.py
+++ HW4/B10830020_洪宇辰/RSA.py
@@ -2,12 +2,15 @@ import math
 import argparse
 import random
 import base64
+import re
 
 
-def get_random(n):
+# genertate a random number for a given digit
+def get_random(n: int):
     return random.randrange(2**(n - 1) + 1, 2**n - 1)
 
 
+# prime list before 350, for low level prime number test
 first_primes_list = [
     2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
     73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151,
@@ -17,6 +20,7 @@ first_primes_list = [
 ]
 
 
+# filter is input number are relative prime with prime number in first_primes_list
 def is_low_level_passed(input: int):
     for divisor in first_primes_list:
         if input % divisor == 0 and divisor**2 <= input:
@@ -25,6 +29,7 @@ def is_low_level_passed(input: int):
         return True
 
 
+# filter input by Miller Rabin test
 def is_high_level_passed(input: int):
     max_div = 0
     e = input - 1
@@ -49,7 +54,8 @@ def is_high_level_passed(input: int):
     return True
 
 
-def generate_2_prime(n):
+# generate two prime number which passed all test and not same with an given digit n
+def generate_2_prime(n: int):
 
     while (True):
         p = get_random(n)
@@ -65,6 +71,7 @@ def generate_2_prime(n):
     return p, q
 
 
+# RSA generate [p,q,phi,e,d] with 1024 digit and print them all
 def rsa_generate_key():
     p, q = generate_2_prime(1024)
     n = p * q
@@ -94,25 +101,30 @@ def rsa_generate_key():
     return key
 
 
+# RSA encryption by given plaintext and key generate from rsa_generate_key
 def rsa_encrypt(plaintext: str, n: int, e: int):
 
     plain_bytes = str.encode(plaintext)
     cipher_bytes = [hex(pow(plain_byte, e, n)) for plain_byte in plain_bytes]
-    cipher_bytes = ",".join(cipher_bytes)
+    cipher_bytes = "".join(cipher_bytes)
     cipher_base64 = base64.b64encode(
         cipher_bytes.encode("ascii")).decode("ascii")
     return cipher_base64
 
 
+# RSA decrption by given ciphertext(in base64) and private key generate from rsa_generate_key
 def rsa_decrypt(cipher_base64: str, n: int, d: int):
     ciphertext = base64.b64decode(
         cipher_base64.encode("ascii")).decode("ascii")
-    cipher_bytes = ciphertext.split(",")
+
+    cipher_bytes = ["0x" + part for part in ciphertext.split("0x") if part]
+
     plaintext = [chr(pow(int(byte, 16), d, n)) for byte in cipher_bytes]
     plaintext = "".join(plaintext)
     return plaintext
 
 
+# RSA decrption in CRT mode by given ciphertext(in base64) and p,q,d generate from rsa_generate_key
 def rsa_crt(cipher_base64: str, p: int, q: int, d: int):
     dp = pow(d, 1, (p - 1))
     dq = pow(d, 1, (q - 1))
@@ -127,7 +139,7 @@ def rsa_crt(cipher_base64: str, p: int, q: int, d: int):
 
     ciphertext = base64.b64decode(
         cipher_base64.encode("ascii")).decode("ascii")
-    cipher_bytes = ciphertext.split(",")
+    cipher_bytes = ["0x" + part for part in ciphertext.split("0x") if part]
     plaintext = [chr(crt(int(byte, 16))) for byte in cipher_bytes]
     plaintext = "".join(plaintext)
     return plaintext
@@ -168,8 +180,10 @@ def get_parser():
 
 def main():
     args = get_parser().parse_args()
+    # if get -i parameter
     if (args.init):
         rsa_generate_key()
+    # if get -e parameter
     if (args.encrypt):
         print(
             rsa_encrypt(
@@ -177,6 +191,7 @@ def main():
                 int(args.encrypt[1]),
                 int(args.encrypt[2]),
             ))
+    # if get -d parameter
     if (args.decrypt):
         print(
             rsa_decrypt(
@@ -184,6 +199,7 @@ def main():
                 int(args.decrypt[1]),
                 int(args.decrypt[2]),
             ))
+    # if get -CRT parameter
     if (args.CRT):
         print(
             rsa_crt(
@@ -196,11 +212,14 @@ def main():
 
 if __name__ == "__main__":
     main()
-    # key = rsa_generate_key()
+
+    ## test program
+
+    key = rsa_generate_key()
     # ciphertext = rsa_encrypt('Hello RSA by b10830020', key['N'], key['e'])
     # print(ciphertext)
     # plaintext = rsa_decrypt(ciphertext, key['N'], key['d'])
     # print(plaintext)
     # plaintext = rsa_crt(ciphertext, key['p'], key['q'], key['d'])
-    # print(plaintext)
     # print(ciphertext, key['p'], key['q'], key['d'])
+    # print(plaintext)
